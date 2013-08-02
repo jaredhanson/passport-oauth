6e0a016a2831e1a70e5dc69cd0c8e862506c0bd9
Jared Hanson <jaredhanson@gmail.com>
2013-08-02 08:28:40 -0700

    Test case for options checking.

--- test/strategies/oauth2.test.js
+++ test/strategies/oauth2.test.js
@@ -22,7 +22,47 @@ describe('OAuth2Strategy', function() {
         clientID: 'ABC123',
         clientSecret: 'secret'
       });
-    }).to.throw(TypeError, 'passport-oauth.OAuth2Strategy requires a verify callback');
+    }).to.throw(TypeError, 'OAuth2Strategy requires a verify callback');
+  });
+  
+  describe('constructed without a authorizationURL option', function() {
+    expect(function() {
+      new OAuth2Strategy({
+        tokenURL: 'https://www.example.com/oauth2/token',
+        clientID: 'ABC123',
+        clientSecret: 'secret'
+      }, function() {});
+    }).to.throw(TypeError, 'OAuth2Strategy requires a authorizationURL option');
+  });
+  
+  describe('constructed without a tokenURL option', function() {
+    expect(function() {
+      new OAuth2Strategy({
+        authorizationURL: 'https://www.example.com/oauth2/authorize',
+        clientID: 'ABC123',
+        clientSecret: 'secret'
+      }, function() {});
+    }).to.throw(TypeError, 'OAuth2Strategy requires a tokenURL option');
+  });
+  
+  describe('constructed without a clientID option', function() {
+    expect(function() {
+      new OAuth2Strategy({
+        authorizationURL: 'https://www.example.com/oauth2/authorize',
+        tokenURL: 'https://www.example.com/oauth2/token',
+        clientSecret: 'secret'
+      }, function() {});
+    }).to.throw(TypeError, 'OAuth2Strategy requires a clientID option');
+  });
+  
+  describe('constructed without a clientSecret option', function() {
+    expect(function() {
+      new OAuth2Strategy({
+        authorizationURL: 'https://www.example.com/oauth2/authorize',
+        tokenURL: 'https://www.example.com/oauth2/token',
+        clientID: 'ABC123'
+      }, function() {});
+    }).to.throw(TypeError, 'OAuth2Strategy requires a clientSecret option');
   });
   
 });
