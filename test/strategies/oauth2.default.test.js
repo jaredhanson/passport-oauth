9fa7459c17f0b5f1cfbc5875e1044fe60c9b9a3f
Jared Hanson <jaredhanson@gmail.com>
2013-08-12 18:06:07 -0700

    Clean up OAuth 2.0 normal and options test cases.

--- test/strategies/oauth2.default.test.js
+++ test/strategies/oauth2.default.test.js
@@ -3,7 +3,7 @@ var chai = require('chai')
   , AuthorizationError = require('../../lib/errors/authorizationerror');
 
 
-describe('OAuth2Strategy with default options', function() {
+describe('OAuth2Strategy', function() {
     
   var strategy = new OAuth2Strategy({
       authorizationURL: 'https://www.example.com/oauth2/authorize',
@@ -13,26 +13,28 @@ describe('OAuth2Strategy with default options', function() {
       callbackURL: 'https://www.example.net/auth/example/callback',
     },
     function(accessToken, refreshToken, profile, done) {
-      if (accessToken == '2YotnFZFEjr1zCsicMWpAA' && refreshToken == 'tGzv3JOkF0XG5Qx2TlKWIA' && Object.keys(profile).length == 0) { 
+      if (Object.keys(profile).length !== 0) { return done(null, false); }
+      
+      if (accessToken == '2YotnFZFEjr1zCsicMWpAA' && refreshToken == 'tGzv3JOkF0XG5Qx2TlKWIA') { 
         return done(null, { id: '1234' }, { message: 'Hello' });
-      } else if (accessToken == 'one-2YotnFZFEjr1zCsicMWpAA' && refreshToken == 'one-tGzv3JOkF0XG5Qx2TlKWIA' && Object.keys(profile).length == 0) { 
-        return done(null, { id: 'one' }, { message: 'Hello' });
-      } else if (accessToken == 'two-2YotnFZFEjr1zCsicMWpAA' && refreshToken == 'two-tGzv3JOkF0XG5Qx2TlKWIA' && Object.keys(profile).length == 0) { 
-        return done(null, { id: 'two' }, { message: 'Hello' });
+      } else if (accessToken == '2YotnFZFEjr1zCsicMWpAA+ALT1' && refreshToken == 'tGzv3JOkF0XG5Qx2TlKWIA+ALT1') { 
+        return done(null, { id: '2234' }, { message: 'Hello' });
+      } else if (accessToken == '2YotnFZFEjr1zCsicMWpAA+ALT2' && refreshToken == 'tGzv3JOkF0XG5Qx2TlKWIA+ALT2') { 
+        return done(null, { id: '3234' }, { message: 'Hello' });
       }
       return done(null, false);
     });
   
   // inject a "mock" oauth2 instance
   strategy._oauth2.getOAuthAccessToken = function(code, options, callback) {
-    if (code == 'SplxlOBeZQQYbYS6WxSbIA' && options.grant_type == 'authorization_code') {
-      if (options.redirect_uri == 'https://www.example.net/auth/example/callback') {
-        return callback(null, '2YotnFZFEjr1zCsicMWpAA', 'tGzv3JOkF0XG5Qx2TlKWIA', { token_type: 'example', expires_in: 3600, example_parameter: 'example_value' });
-      } else if (options.redirect_uri == 'https://www.example.net/auth/example/one-callback') {
-        return callback(null, 'one-2YotnFZFEjr1zCsicMWpAA', 'one-tGzv3JOkF0XG5Qx2TlKWIA', { token_type: 'example', expires_in: 3600, example_parameter: 'example_value' });
-      } else if (options.redirect_uri == 'https://www.example.net/auth/example/two-callback') {
-        return callback(null, 'two-2YotnFZFEjr1zCsicMWpAA', 'two-tGzv3JOkF0XG5Qx2TlKWIA', { token_type: 'example', expires_in: 3600, example_parameter: 'example_value' });
-      }
+    if (options.grant_type !== 'authorization_code') { return callback(null, 'wrong-access-token', 'wrong-refresh-token'); }
+    
+    if (code == 'SplxlOBeZQQYbYS6WxSbIA' && options.redirect_uri == 'https://www.example.net/auth/example/callback') {
+      return callback(null, '2YotnFZFEjr1zCsicMWpAA', 'tGzv3JOkF0XG5Qx2TlKWIA', { token_type: 'example' });
+    } else if (code == 'SplxlOBeZQQYbYS6WxSbIA+ALT1' && options.redirect_uri == 'https://www.example.net/auth/example/callback/alt1') {
+      return callback(null, '2YotnFZFEjr1zCsicMWpAA+ALT1', 'tGzv3JOkF0XG5Qx2TlKWIA+ALT1', { token_type: 'example' });
+    } else if (code == 'SplxlOBeZQQYbYS6WxSbIA+ALT2' && options.redirect_uri == 'https://www.example.net/auth/example/callback/alt2') {
+      return callback(null, '2YotnFZFEjr1zCsicMWpAA+ALT2', 'tGzv3JOkF0XG5Qx2TlKWIA+ALT2', { token_type: 'example' });
     } else {
       return callback(null, 'wrong-access-token', 'wrong-refresh-token');
     }
@@ -67,7 +69,7 @@ describe('OAuth2Strategy with default options', function() {
     });
   });
   
-  describe('handling an authorized return request with callbackURL option override', function() {
+  describe('handling an authorized return request with callbackURL option', function() {
     var user
       , info;
   
@@ -80,14 +82,14 @@ describe('OAuth2Strategy with default options', function() {
         })
         .req(function(req) {
           req.query = {};
-          req.query.code = 'SplxlOBeZQQYbYS6WxSbIA';
+          req.query.code = 'SplxlOBeZQQYbYS6WxSbIA+ALT1';
         })
-        .authenticate({ callbackURL: 'https://www.example.net/auth/example/one-callback' });
+        .authenticate({ callbackURL: 'https://www.example.net/auth/example/callback/alt1' });
     });
   
     it('should supply user', function() {
       expect(user).to.be.an.object;
-      expect(user.id).to.equal('one');
+      expect(user.id).to.equal('2234');
     });
   
     it('should supply info', function() {
@@ -96,7 +98,7 @@ describe('OAuth2Strategy with default options', function() {
     });
   });
   
-  describe('handling an authorized return request with a relative callbackURL option override', function() {
+  describe('handling an authorized return request with relative callbackURL option', function() {
     var user
       , info;
   
@@ -108,18 +110,18 @@ describe('OAuth2Strategy with default options', function() {
           done();
         })
         .req(function(req) {
-          req.url = '/auth/example/two-callback';
+          req.url = '/auth/example/callback/alt2';
           req.headers.host = 'www.example.net';
           req.query = {};
-          req.query.code = 'SplxlOBeZQQYbYS6WxSbIA';
+          req.query.code = 'SplxlOBeZQQYbYS6WxSbIA+ALT2';
           req.connection = { encrypted: true };
         })
-        .authenticate({ callbackURL: '/auth/example/two-callback' });
+        .authenticate({ callbackURL: '/auth/example/callback/alt2' });
     });
   
     it('should supply user', function() {
       expect(user).to.be.an.object;
-      expect(user.id).to.equal('two');
+      expect(user.id).to.equal('3234');
     });
   
     it('should supply info', function() {
@@ -147,7 +149,7 @@ describe('OAuth2Strategy with default options', function() {
     });
   });
   
-  describe('handling a request to be redirected for authorization with a scope', function() {
+  describe('handling a request to be redirected for authorization with scope option', function() {
     var url;
   
     before(function(done) {
@@ -158,15 +160,15 @@ describe('OAuth2Strategy with default options', function() {
         })
         .req(function(req) {
         })
-        .authenticate({ scope: 'permission' });
+        .authenticate({ scope: 'email' });
     });
   
     it('should be redirected', function() {
-      expect(url).to.equal('https://www.example.com/oauth2/authorize?response_type=code&redirect_uri=https%3A%2F%2Fwww.example.net%2Fauth%2Fexample%2Fcallback&scope=permission&client_id=ABC123&type=web_server');
+      expect(url).to.equal('https://www.example.com/oauth2/authorize?response_type=code&redirect_uri=https%3A%2F%2Fwww.example.net%2Fauth%2Fexample%2Fcallback&scope=email&client_id=ABC123&type=web_server');
     });
   });
   
-  describe('handling a request to be redirected for authorization with multiple scopes', function() {
+  describe('handling a request to be redirected for authorization with scope option as array', function() {
     var url;
   
     before(function(done) {
@@ -185,7 +187,7 @@ describe('OAuth2Strategy with default options', function() {
     });
   });
   
-  describe('handling a request to be redirected for authorization with callbackURL option override', function() {
+  describe('handling a request to be redirected for authorization with callbackURL option', function() {
     var url;
   
     before(function(done) {
@@ -196,15 +198,15 @@ describe('OAuth2Strategy with default options', function() {
         })
         .req(function(req) {
         })
-        .authenticate({ callbackURL: 'https://www.example.net/auth/example/one-callback' });
+        .authenticate({ callbackURL: 'https://www.example.net/auth/example/callback/alt1' });
     });
   
     it('should be redirected', function() {
-      expect(url).to.equal('https://www.example.com/oauth2/authorize?response_type=code&redirect_uri=https%3A%2F%2Fwww.example.net%2Fauth%2Fexample%2Fone-callback&client_id=ABC123&type=web_server');
+      expect(url).to.equal('https://www.example.com/oauth2/authorize?response_type=code&redirect_uri=https%3A%2F%2Fwww.example.net%2Fauth%2Fexample%2Fcallback%2Falt1&client_id=ABC123&type=web_server');
     });
   });
   
-  describe('handling a request to be redirected for authorization with a relative callbackURL option override', function() {
+  describe('handling a request to be redirected for authorization with relative callbackURL option', function() {
     var url;
   
     before(function(done) {
@@ -214,19 +216,19 @@ describe('OAuth2Strategy with default options', function() {
           done();
         })
         .req(function(req) {
-          req.url = '/auth/example/two-callback';
+          req.url = '/auth/example/callback/alt2';
           req.headers.host = 'www.example.net';
           req.connection = { encrypted: true };
         })
-        .authenticate({ callbackURL: '/auth/example/two-callback' });
+        .authenticate({ callbackURL: '/auth/example/callback/alt2' });
     });
   
     it('should be redirected', function() {
-      expect(url).to.equal('https://www.example.com/oauth2/authorize?response_type=code&redirect_uri=https%3A%2F%2Fwww.example.net%2Fauth%2Fexample%2Ftwo-callback&client_id=ABC123&type=web_server');
+      expect(url).to.equal('https://www.example.com/oauth2/authorize?response_type=code&redirect_uri=https%3A%2F%2Fwww.example.net%2Fauth%2Fexample%2Fcallback%2Falt2&client_id=ABC123&type=web_server');
     });
   });
   
-  describe('handling a request that has been denied by the user without description', function() {
+  describe('handling a return request in which authorization has been denied by the user without description', function() {
     var info;
   
     before(function(done) {
@@ -242,13 +244,13 @@ describe('OAuth2Strategy with default options', function() {
         .authenticate();
     });
   
-    it('should supply info', function() {
+    it('should fail without message', function() {
       expect(info).to.not.be.undefined;
       expect(info.message).to.be.undefined;
     });
   });
   
-  describe('handling a request that has been denied by the user with description', function() {
+  describe('handling a return request in which authorization has been denied by the user with description', function() {
     var info;
   
     before(function(done) {
@@ -271,7 +273,7 @@ describe('OAuth2Strategy with default options', function() {
     });
   });
   
-  describe('handling a request that indicates a server error without description', function() {
+  describe('handling a return request that indicates server error without description', function() {
     var err;
   
     before(function(done) {
@@ -296,7 +298,7 @@ describe('OAuth2Strategy with default options', function() {
     });
   });
   
-  describe('handling a request that indicates a server error with description', function() {
+  describe('handling a return request that indicates server error with description', function() {
     var err;
   
     before(function(done) {
@@ -322,7 +324,7 @@ describe('OAuth2Strategy with default options', function() {
     });
   });
   
-  describe('handling a request that indicates a server error with description and link', function() {
+  describe('handling a return request that indicates server error with description and link', function() {
     var err;
   
     before(function(done) {
@@ -345,171 +347,6 @@ describe('OAuth2Strategy with default options', function() {
       expect(err.message).to.equal('The scope is invalid');
       expect(err.uri).to.equal('http://www.example.com/oauth2/help');
       expect(err.status).to.equal(500);
-    });
-  });
-  
-});
-
-
-describe('OAuth2Strategy with default options using relative callback URL', function() {
-    
-  var strategy = new OAuth2Strategy({
-      authorizationURL: 'https://www.example.com/oauth2/authorize',
-      tokenURL: 'https://www.example.com/oauth2/token',
-      clientID: 'ABC123',
-      clientSecret: 'secret',
-      callbackURL: '/auth/example/callback',
-    },
-    function(accessToken, refreshToken, profile, done) {
-      if (accessToken == '2YotnFZFEjr1zCsicMWpAA' && refreshToken == 'tGzv3JOkF0XG5Qx2TlKWIA' && Object.keys(profile).length == 0) { 
-        return done(null, { id: '1234' }, { message: 'Hello' });
-      } else if (accessToken == 'insecure-2YotnFZFEjr1zCsicMWpAA' && refreshToken == 'insecure-tGzv3JOkF0XG5Qx2TlKWIA' && Object.keys(profile).length == 0) { 
-        return done(null, { id: '1234' }, { message: 'Insecure Hello' });
-      }
-      return done(null, false);
-    });
-  
-  // inject a "mock" oauth2 instance
-  strategy._oauth2.getOAuthAccessToken = function(code, options, callback) {
-    if (code == 'SplxlOBeZQQYbYS6WxSbIA' && options.grant_type == 'authorization_code' &&
-        options.redirect_uri == 'https://www.example.net/auth/example/callback') {
-      return callback(null, '2YotnFZFEjr1zCsicMWpAA', 'tGzv3JOkF0XG5Qx2TlKWIA', { token_type: 'example', expires_in: 3600, example_parameter: 'example_value' });
-    } else if (code == 'SplxlOBeZQQYbYS6WxSbIA' && options.grant_type == 'authorization_code' &&
-        options.redirect_uri == 'http://www.example.net/auth/example/callback') {
-      return callback(null, 'insecure-2YotnFZFEjr1zCsicMWpAA', 'insecure-tGzv3JOkF0XG5Qx2TlKWIA', { token_type: 'example', expires_in: 3600, example_parameter: 'example_value' });
-    } else {
-      return callback(null, 'wrong-access-token', 'wrong-refresh-token');
-    }
-  }
-  
-  describe('handling an authorized return request on secure connection', function() {
-    var user
-      , info;
-  
-    before(function(done) {
-      chai.passport(strategy)
-        .success(function(u, i) {
-          user = u;
-          info = i;
-          done();
-        })
-        .req(function(req) {
-          req.url = '/auth/example';
-          req.headers.host = 'www.example.net';
-          req.query = {};
-          req.query.code = 'SplxlOBeZQQYbYS6WxSbIA';
-          req.connection = { encrypted: true };
-        })
-        .authenticate();
-    });
-  
-    it('should supply user', function() {
-      expect(user).to.be.an.object;
-      expect(user.id).to.equal('1234');
-    });
-  
-    it('should supply info', function() {
-      expect(info).to.be.an.object;
-      expect(info.message).to.equal('Hello');
-    });
-  });
-  
-  describe('handling an authorized return request on insecure connection', function() {
-    var user
-      , info;
-  
-    before(function(done) {
-      chai.passport(strategy)
-        .success(function(u, i) {
-          user = u;
-          info = i;
-          done();
-        })
-        .req(function(req) {
-          req.url = '/auth/example';
-          req.headers.host = 'www.example.net';
-          req.query = {};
-          req.query.code = 'SplxlOBeZQQYbYS6WxSbIA';
-          req.connection = {};
-        })
-        .authenticate();
-    });
-  
-    it('should supply user', function() {
-      expect(user).to.be.an.object;
-      expect(user.id).to.equal('1234');
-    });
-  
-    it('should supply info', function() {
-      expect(info).to.be.an.object;
-      expect(info.message).to.equal('Insecure Hello');
-    });
-  });
-  
-  describe('handling a request on secure connection to be redirected for authorization', function() {
-    var url;
-  
-    before(function(done) {
-      chai.passport(strategy)
-        .redirect(function(u) {
-          url = u;
-          done();
-        })
-        .req(function(req) {
-          req.url = '/auth/example';
-          req.headers.host = 'www.example.net';
-          req.connection = { encrypted: true };
-        })
-        .authenticate();
-    });
-  
-    it('should be redirected', function() {
-      expect(url).to.equal('https://www.example.com/oauth2/authorize?response_type=code&redirect_uri=https%3A%2F%2Fwww.example.net%2Fauth%2Fexample%2Fcallback&client_id=ABC123&type=web_server');
-    });
-  });
-  
-  describe('handling a request on insecure connection to be redirected for authorization', function() {
-    var url;
-  
-    before(function(done) {
-      chai.passport(strategy)
-        .redirect(function(u) {
-          url = u;
-          done();
-        })
-        .req(function(req) {
-          req.url = '/auth/example';
-          req.headers.host = 'www.example.net';
-          req.connection = {};
-        })
-        .authenticate();
-    });
-  
-    it('should be redirected', function() {
-      expect(url).to.equal('https://www.example.com/oauth2/authorize?response_type=code&redirect_uri=http%3A%2F%2Fwww.example.net%2Fauth%2Fexample%2Fcallback&client_id=ABC123&type=web_server');
-    });
-  });
-  
-  describe('handling a request from behind a proxy to be redirected for authorization', function() {
-    var url;
-  
-    before(function(done) {
-      chai.passport(strategy)
-        .redirect(function(u) {
-          url = u;
-          done();
-        })
-        .req(function(req) {
-          req.url = '/auth/example';
-          req.headers.host = 'www.example.net';
-          req.headers['x-forwarded-proto'] = 'https';
-          req.connection = {};
-        })
-        .authenticate();
-    });
-  
-    it('should be redirected', function() {
-      expect(url).to.equal('https://www.example.com/oauth2/authorize?response_type=code&redirect_uri=https%3A%2F%2Fwww.example.net%2Fauth%2Fexample%2Fcallback&client_id=ABC123&type=web_server');
     });
   });
   
