2326a3d250fa922b45febea2934983141f5c81da
Jared Hanson <jaredhanson@gmail.com>
2013-08-12 19:47:59 -0700

    Clean up OAuth 2.0 pass request test cases.

--- test/strategies/oauth2.passreq.test.js
+++ test/strategies/oauth2.passreq.test.js
@@ -2,9 +2,9 @@ var chai = require('chai')
   , OAuth2Strategy = require('../../lib/strategies/oauth2');
 
 
-describe('OAuth2Strategy with passReqToCallback option', function() {
+describe('OAuth2Strategy', function() {
   
-  describe('and standard verify callback', function() {
+  describe('passing request to verify callback', function() {
   
     var strategy = new OAuth2Strategy({
         authorizationURL: 'https://www.example.com/oauth2/authorize',
@@ -15,7 +15,9 @@ describe('OAuth2Strategy with passReqToCallback option', function() {
         passReqToCallback: true
       },
       function(req, accessToken, refreshToken, profile, done) {
-        if (accessToken == '2YotnFZFEjr1zCsicMWpAA' && refreshToken == 'tGzv3JOkF0XG5Qx2TlKWIA' && Object.keys(profile).length == 0) { 
+        if (Object.keys(profile).length !== 0) { return done(null, false); }
+        
+        if (accessToken == '2YotnFZFEjr1zCsicMWpAA' && refreshToken == 'tGzv3JOkF0XG5Qx2TlKWIA') { 
           return done(null, { id: '1234' }, { message: 'Hello', foo: req.headers['x-foo'] });
         }
         return done(null, false);
@@ -23,10 +25,10 @@ describe('OAuth2Strategy with passReqToCallback option', function() {
   
     // inject a "mock" oauth2 instance
     strategy._oauth2.getOAuthAccessToken = function(code, options, callback) {
-      if (code == 'SplxlOBeZQQYbYS6WxSbIA' &&
-          options.grant_type == 'authorization_code' &&
-          options.redirect_uri == 'https://www.example.net/auth/example/callback') {
-        callback(null, '2YotnFZFEjr1zCsicMWpAA', 'tGzv3JOkF0XG5Qx2TlKWIA', { token_type: 'example', expires_in: 3600, example_parameter: 'example_value' });
+      if (options.grant_type !== 'authorization_code') { return callback(null, 'wrong-access-token', 'wrong-refresh-token'); }
+      
+      if (code == 'SplxlOBeZQQYbYS6WxSbIA' && options.redirect_uri == 'https://www.example.net/auth/example/callback') {
+        callback(null, '2YotnFZFEjr1zCsicMWpAA', 'tGzv3JOkF0XG5Qx2TlKWIA', { token_type: 'example' });
       } else {
         callback(null, 'wrong-access-token', 'wrong-refresh-token');
       }
@@ -67,7 +69,7 @@ describe('OAuth2Strategy with passReqToCallback option', function() {
     });
   });
   
-  describe('and params argument in verify callback', function() {
+  describe('passing request to verify callback that accepts params', function() {
   
     var strategy = new OAuth2Strategy({
         authorizationURL: 'https://www.example.com/oauth2/authorize',
@@ -78,7 +80,10 @@ describe('OAuth2Strategy with passReqToCallback option', function() {
         passReqToCallback: true
       },
       function(req, accessToken, refreshToken, params, profile, done) {
-        if (accessToken == '2YotnFZFEjr1zCsicMWpAA' && refreshToken == 'tGzv3JOkF0XG5Qx2TlKWIA' && params.example_parameter == 'example_value' && Object.keys(profile).length == 0) { 
+        if (params.example_parameter !== 'example_value') { return done(null, false); }
+        if (Object.keys(profile).length !== 0) { return done(null, false); }
+        
+        if (accessToken == '2YotnFZFEjr1zCsicMWpAA' && refreshToken == 'tGzv3JOkF0XG5Qx2TlKWIA') { 
           return done(null, { id: '1234' }, { message: 'Hello', foo: req.headers['x-foo'] });
         }
         return done(null, false);
@@ -86,9 +91,9 @@ describe('OAuth2Strategy with passReqToCallback option', function() {
   
     // inject a "mock" oauth2 instance
     strategy._oauth2.getOAuthAccessToken = function(code, options, callback) {
-      if (code == 'SplxlOBeZQQYbYS6WxSbIA' &&
-          options.grant_type == 'authorization_code' &&
-          options.redirect_uri == 'https://www.example.net/auth/example/callback') {
+      if (options.grant_type !== 'authorization_code') { return callback(null, 'wrong-access-token', 'wrong-refresh-token'); }
+      
+      if (code == 'SplxlOBeZQQYbYS6WxSbIA' && options.redirect_uri == 'https://www.example.net/auth/example/callback') {
         callback(null, '2YotnFZFEjr1zCsicMWpAA', 'tGzv3JOkF0XG5Qx2TlKWIA', { token_type: 'example', expires_in: 3600, example_parameter: 'example_value' });
       } else {
         callback(null, 'wrong-access-token', 'wrong-refresh-token');
