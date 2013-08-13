99c4aa5c201d80b281f613427922f333a51f9924
Jared Hanson <jaredhanson@gmail.com>
2013-08-12 22:28:55 -0700

    Clean up OAuth options test case.

--- test/strategies/oauth.default.test.js
+++ test/strategies/oauth.default.test.js
@@ -165,65 +165,6 @@ describe('OAuthStrategy', function() {
       expect(request.session['oauth']).to.be.undefined;
     });
   });
-
-
-  describe('with user authorization URL that contains query parameters', function() {
-    var strategy = new OAuthStrategy({
-        requestTokenURL: 'https://www.example.com/oauth/request_token',
-        accessTokenURL: 'https://www.example.com/oauth/access_token',
-        userAuthorizationURL: 'https://www.example.com/oauth/authorize?foo=bar',
-        consumerKey: 'ABC123',
-        consumerSecret: 'secret'
-      }, function(token, tokenSecret, profile, done) {
-        if (Object.keys(profile).length !== 0) { return done(null, false); }
-        
-        if (token == 'nnch734d00sl2jdk' && tokenSecret == 'pfkkdhi9sl3r4s00') {
-          return done(null, { id: '1234' }, { message: 'Hello' });
-        }
-        return done(null, false);
-      });
-    
-    // inject a "mock" oauth instance
-    strategy._oauth.getOAuthAccessToken = function(token, tokenSecret, verifier, callback) {
-      if (token == 'hh5s93j4hdidpola' && tokenSecret == 'hdhd0244k9j7ao03' && verifier == 'hfdp7dh39dks9884') {
-        return callback(null, 'nnch734d00sl2jdk', 'pfkkdhi9sl3r4s00', {});
-      } else {
-        return callback(null, 'wrong-token', 'wrong-token-secret');
-      }
-    }
-  
-    strategy._oauth.getOAuthRequestToken = function(extraParams, callback) {
-      callback(null, 'hh5s93j4hdidpola', 'hdhd0244k9j7ao03', {});
-    }
-  
-    describe('handling a request to be redirected after obtaining a request token', function() {
-      var request
-        , url;
-
-      before(function(done) {
-        chai.passport(strategy)
-          .redirect(function(u) {
-            url = u;
-            done();
-          })
-          .req(function(req) {
-            request = req;
-            req.session = {};
-          })
-          .authenticate();
-      });
-
-      it('should be redirected', function() {
-        expect(url).to.equal('https://www.example.com/oauth/authorize?foo=bar&oauth_token=hh5s93j4hdidpola');
-      });
-    
-      it('should store token and token secret in session', function() {
-        expect(request.session['oauth']).to.not.be.undefined;
-        expect(request.session['oauth']['oauth_token']).to.equal('hh5s93j4hdidpola');
-        expect(request.session['oauth']['oauth_token_secret']).to.equal('hdhd0244k9j7ao03');
-      });
-    });
-  });
   
 });
 
--- test/strategies/oauth.options.test.js
+++ test/strategies/oauth.options.test.js
@@ -5,7 +5,6 @@ var chai = require('chai')
 describe('OAuthStrategy', function() {
     
   describe('with callback URL option', function() {
-    
     var strategy = new OAuthStrategy({
         requestTokenURL: 'https://www.example.com/oauth/request_token',
         accessTokenURL: 'https://www.example.com/oauth/access_token',
@@ -16,6 +15,10 @@ describe('OAuthStrategy', function() {
       }, function(token, tokenSecret, profile, done) {
         if (token == 'nnch734d00sl2jdk' && tokenSecret == 'pfkkdhi9sl3r4s00') {
           return done(null, { id: '1234', profile: profile }, { message: 'Hello' });
+        } else if (token == 'nnch734d00sl2jdk+alt1' && tokenSecret == 'pfkkdhi9sl3r4s00+alt1') {
+          return done(null, { id: '2234', profile: profile }, { message: 'Hello' });
+        } else if (token == 'nnch734d00sl2jdk-alt1' && tokenSecret == 'pfkkdhi9sl3r4s00-alt1') {
+          return done(null, { id: '3234', profile: profile }, { message: 'Hello' });
         }
         return done(null, false);
       });
@@ -24,6 +27,10 @@ describe('OAuthStrategy', function() {
     strategy._oauth.getOAuthAccessToken = function(token, tokenSecret, verifier, callback) {
       if (token == 'hh5s93j4hdidpola' && tokenSecret == 'hdhd0244k9j7ao03' && verifier == 'hfdp7dh39dks9884') {
         return callback(null, 'nnch734d00sl2jdk', 'pfkkdhi9sl3r4s00', {});
+      } else if (token == 'hh5s93j4hdidpola+alt1' && tokenSecret == 'hdhd0244k9j7ao03+alt1' && verifier == 'hfdp7dh39dks9884') {
+        return callback(null, 'nnch734d00sl2jdk+alt1', 'pfkkdhi9sl3r4s00+alt1', {});
+      } else if (token == 'hh5s93j4hdidpola-alt2' && tokenSecret == 'hdhd0244k9j7ao03-alt2' && verifier == 'hfdp7dh39dks9884') {
+        return callback(null, 'nnch734d00sl2jdk-alt2', 'pfkkdhi9sl3r4s00-alt2', {});
       } else {
         return callback(null, 'wrong-token', 'wrong-token-secret');
       }
@@ -32,6 +39,10 @@ describe('OAuthStrategy', function() {
     strategy._oauth.getOAuthRequestToken = function(extraParams, callback) {
       if (extraParams.oauth_callback == 'https://www.example.net/auth/example/callback') {
         callback(null, 'hh5s93j4hdidpola', 'hdhd0244k9j7ao03', {});
+      } else if (extraParams.oauth_callback == 'https://www.example.net/auth/example/callback/alt1') {
+        callback(null, 'hh5s93j4hdidpola+alt1', 'hdhd0244k9j7ao03+alt1', {});
+      } else if (extraParams.oauth_callback == 'https://www.example.net/auth/example/callback/alt2') {
+        callback(null, 'hh5s93j4hdidpola-alt2', 'hdhd0244k9j7ao03-alt2', {});
       } else {
         callback(new Error('wrong request token params'));
       }
@@ -64,42 +75,8 @@ describe('OAuthStrategy', function() {
         expect(request.session['oauth']['oauth_token_secret']).to.equal('hdhd0244k9j7ao03');
       });
     });
-  });
-  
-  describe('with callback URL option that is overridden', function() {
     
-    var strategy = new OAuthStrategy({
-        requestTokenURL: 'https://www.example.com/oauth/request_token',
-        accessTokenURL: 'https://www.example.com/oauth/access_token',
-        userAuthorizationURL: 'https://www.example.com/oauth/authorize',
-        consumerKey: 'ABC123',
-        consumerSecret: 'secret',
-        callbackURL: 'https://www.example.net/auth/example/callback'
-      }, function(token, tokenSecret, profile, done) {
-        if (token == 'nnch734d00sl2jdk' && tokenSecret == 'pfkkdhi9sl3r4s00') {
-          return done(null, { id: '1234', profile: profile }, { message: 'Hello' });
-        }
-        return done(null, false);
-      });
-    
-    // inject a "mock" oauth instance
-    strategy._oauth.getOAuthAccessToken = function(token, tokenSecret, verifier, callback) {
-      if (token == 'hh5s93j4hdidpola' && tokenSecret == 'hdhd0244k9j7ao03' && verifier == 'hfdp7dh39dks9884') {
-        return callback(null, 'nnch734d00sl2jdk', 'pfkkdhi9sl3r4s00', {});
-      } else {
-        return callback(null, 'wrong-token', 'wrong-token-secret');
-      }
-    }
-    
-    strategy._oauth.getOAuthRequestToken = function(extraParams, callback) {
-      if (extraParams.oauth_callback == 'https://www.example.net/auth/example/other-callback') {
-        callback(null, 'hh5s93j4hdidpola', 'hdhd0244k9j7ao03', {});
-      } else {
-        callback(new Error('wrong request token params'));
-      }
-    }
-    
-    describe('handling a request to be redirected', function() {
+    describe('handling a request to be redirected with callback URL option override', function() {
       var request
         , url;
 
@@ -113,55 +90,21 @@ describe('OAuthStrategy', function() {
             request = req;
             req.session = {};
           })
-          .authenticate({ callbackURL: 'https://www.example.net/auth/example/other-callback' });
+          .authenticate({ callbackURL: 'https://www.example.net/auth/example/callback/alt1' });
       });
 
       it('should be redirected', function() {
-        expect(url).to.equal('https://www.example.com/oauth/authorize?oauth_token=hh5s93j4hdidpola');
+        expect(url).to.equal('https://www.example.com/oauth/authorize?oauth_token=hh5s93j4hdidpola%2Balt1');
       });
     
       it('should store token and token secret in session', function() {
         expect(request.session['oauth']).to.not.be.undefined;
-        expect(request.session['oauth']['oauth_token']).to.equal('hh5s93j4hdidpola');
-        expect(request.session['oauth']['oauth_token_secret']).to.equal('hdhd0244k9j7ao03');
+        expect(request.session['oauth']['oauth_token']).to.equal('hh5s93j4hdidpola+alt1');
+        expect(request.session['oauth']['oauth_token_secret']).to.equal('hdhd0244k9j7ao03+alt1');
       });
     });
-  });
-  
-  describe('with callback URL option that is overridden with relative URL', function() {
     
-    var strategy = new OAuthStrategy({
-        requestTokenURL: 'https://www.example.com/oauth/request_token',
-        accessTokenURL: 'https://www.example.com/oauth/access_token',
-        userAuthorizationURL: 'https://www.example.com/oauth/authorize',
-        consumerKey: 'ABC123',
-        consumerSecret: 'secret',
-        callbackURL: 'https://www.example.net/auth/example/callback'
-      }, function(token, tokenSecret, profile, done) {
-        if (token == 'nnch734d00sl2jdk' && tokenSecret == 'pfkkdhi9sl3r4s00') {
-          return done(null, { id: '1234', profile: profile }, { message: 'Hello' });
-        }
-        return done(null, false);
-      });
-    
-    // inject a "mock" oauth instance
-    strategy._oauth.getOAuthAccessToken = function(token, tokenSecret, verifier, callback) {
-      if (token == 'hh5s93j4hdidpola' && tokenSecret == 'hdhd0244k9j7ao03' && verifier == 'hfdp7dh39dks9884') {
-        return callback(null, 'nnch734d00sl2jdk', 'pfkkdhi9sl3r4s00', {});
-      } else {
-        return callback(null, 'wrong-token', 'wrong-token-secret');
-      }
-    }
-    
-    strategy._oauth.getOAuthRequestToken = function(extraParams, callback) {
-      if (extraParams.oauth_callback == 'https://www.example.net/auth/example/another-callback') {
-        callback(null, 'hh5s93j4hdidpola', 'hdhd0244k9j7ao03', {});
-      } else {
-        callback(new Error('wrong request token params'));
-      }
-    }
-    
-    describe('handling a request to be redirected', function() {
+    describe('handling a request to be redirected with relative callback URL option override', function() {
       var request
         , url;
 
@@ -178,23 +121,22 @@ describe('OAuthStrategy', function() {
             req.session = {};
             req.connection = { encrypted: true };
           })
-          .authenticate({ callbackURL: '/auth/example/another-callback' });
+          .authenticate({ callbackURL: '/auth/example/callback/alt2' });
       });
 
       it('should be redirected', function() {
-        expect(url).to.equal('https://www.example.com/oauth/authorize?oauth_token=hh5s93j4hdidpola');
+        expect(url).to.equal('https://www.example.com/oauth/authorize?oauth_token=hh5s93j4hdidpola-alt2');
       });
     
       it('should store token and token secret in session', function() {
         expect(request.session['oauth']).to.not.be.undefined;
-        expect(request.session['oauth']['oauth_token']).to.equal('hh5s93j4hdidpola');
-        expect(request.session['oauth']['oauth_token_secret']).to.equal('hdhd0244k9j7ao03');
+        expect(request.session['oauth']['oauth_token']).to.equal('hh5s93j4hdidpola-alt2');
+        expect(request.session['oauth']['oauth_token_secret']).to.equal('hdhd0244k9j7ao03-alt2');
       });
     });
   });
   
   describe('with relative callback URL option', function() {
-    
     var strategy = new OAuthStrategy({
         requestTokenURL: 'https://www.example.com/oauth/request_token',
         accessTokenURL: 'https://www.example.com/oauth/access_token',
@@ -226,7 +168,7 @@ describe('OAuthStrategy', function() {
       }
     }
     
-    describe('handling a request to be redirected with params', function() {
+    describe('handling a request to be redirected for authorization', function() {
       var request
         , url;
 
@@ -248,6 +190,64 @@ describe('OAuthStrategy', function() {
 
       it('should be redirected', function() {
         expect(url).to.equal('https://www.example.com/oauth/authorize?oauth_token=hh5s93j4hdidpola');
+      });
+    
+      it('should store token and token secret in session', function() {
+        expect(request.session['oauth']).to.not.be.undefined;
+        expect(request.session['oauth']['oauth_token']).to.equal('hh5s93j4hdidpola');
+        expect(request.session['oauth']['oauth_token_secret']).to.equal('hdhd0244k9j7ao03');
+      });
+    });
+  });
+  
+  describe('with user authorization URL that contains query parameters', function() {
+    var strategy = new OAuthStrategy({
+        requestTokenURL: 'https://www.example.com/oauth/request_token',
+        accessTokenURL: 'https://www.example.com/oauth/access_token',
+        userAuthorizationURL: 'https://www.example.com/oauth/authorize?foo=bar',
+        consumerKey: 'ABC123',
+        consumerSecret: 'secret'
+      }, function(token, tokenSecret, profile, done) {
+        if (Object.keys(profile).length !== 0) { return done(null, false); }
+        
+        if (token == 'nnch734d00sl2jdk' && tokenSecret == 'pfkkdhi9sl3r4s00') {
+          return done(null, { id: '1234' }, { message: 'Hello' });
+        }
+        return done(null, false);
+      });
+    
+    // inject a "mock" oauth instance
+    strategy._oauth.getOAuthAccessToken = function(token, tokenSecret, verifier, callback) {
+      if (token == 'hh5s93j4hdidpola' && tokenSecret == 'hdhd0244k9j7ao03' && verifier == 'hfdp7dh39dks9884') {
+        return callback(null, 'nnch734d00sl2jdk', 'pfkkdhi9sl3r4s00', {});
+      } else {
+        return callback(null, 'wrong-token', 'wrong-token-secret');
+      }
+    }
+  
+    strategy._oauth.getOAuthRequestToken = function(extraParams, callback) {
+      callback(null, 'hh5s93j4hdidpola', 'hdhd0244k9j7ao03', {});
+    }
+  
+    describe('handling a request to be redirected after obtaining a request token', function() {
+      var request
+        , url;
+
+      before(function(done) {
+        chai.passport(strategy)
+          .redirect(function(u) {
+            url = u;
+            done();
+          })
+          .req(function(req) {
+            request = req;
+            req.session = {};
+          })
+          .authenticate();
+      });
+
+      it('should be redirected', function() {
+        expect(url).to.equal('https://www.example.com/oauth/authorize?foo=bar&oauth_token=hh5s93j4hdidpola');
       });
     
       it('should store token and token secret in session', function() {
